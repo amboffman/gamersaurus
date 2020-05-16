e5c405403462ccd003f8e7d099037b242785a997
Anthony Boffman <amarrokalboffman@gmail.com>
2020-05-16 12:50:44 -0700

    Connect to mlab with secured keys

--- config/initDb.js
+++ config/initDb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoUri = process.env.MONGODB_URI || "mongodb://localhost:27017/appDB";
+const mongoUri = process.env.MONGODB_URI || `mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PASS}@ds139956.mlab.com:39956/heroku_5mlp82b4`;
 
 const dbOptions = {
   useNewUrlParser: true,
