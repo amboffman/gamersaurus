76643afc28a7609d99b33d7042eae6d967d17fc5
Samwise <samuelfoster0@gmail.com>
2020-05-18 18:51:47 -0700

    remove camelCase from eslint

--- controllers/igdb.js
+++ controllers/igdb.js
@@ -4,24 +4,23 @@ const axios = require("axios");
 const router = express.Router();
 
 router.get("/igdbgames", (req, res) => {
-  axios
-    ({
-      url: "https://api-v3.igdb.com/games",
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'user-key': process.env.API_Key
-      },
-      data: "fields *; limit 10; sort popularity desc;"
-    })
+  axios({
+    url: "https://api-v3.igdb.com/games",
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "user-key": process.env.API_Key,
+    },
+    data: "fields *; limit 10; sort popularity desc;",
+  })
     .then((response) => {
-      console.log(response.data)
-      const igdbData = createGameFromIGDBData(response.data); 
+      console.log(response.data);
+      const igdbData = createGameFromIGDBData(response.data);
       return res.json(igdbData);
     })
     .catch((error) => {
       // handle errors
-       if (error.response) {
+      if (error.response) {
         console.log(error.response.statusText);
         res.sendStatus(error.response.status);
       } else {
@@ -31,23 +30,35 @@ router.get("/igdbgames", (req, res) => {
 });
 
 function createGameFromIGDBData(IGDBData) {
-  return IGDBData.items
-    .map((item) => {
-      const gameId = item.id;
-      const {
-        age_ratings,
-        cover,
-        first_release_date,
-        genres,
-        name,
-        platforms,
-        rating,
-        screenshots,
-        summary,
-        videos,
-      } = item;
-      return {gameId, age_ratings, cover,nfirst_release_date, genres,first_release_date, name, platforms, rating, screenshots, summary, videos };
-    })
+  return IGDBData.items.map((item) => {
+    const gameId = item.id;
+    const {
+      age_ratings,
+      cover,
+      first_release_date,
+      genres,
+      name,
+      platforms,
+      rating,
+      screenshots,
+      summary,
+      videos,
+    } = item;
+    return {
+      gameId,
+      age_ratings,
+      cover,
+      nfirst_release_date,
+      genres,
+      first_release_date,
+      name,
+      platforms,
+      rating,
+      screenshots,
+      summary,
+      videos,
+    };
+  });
 }
 
 module.exports = router;
