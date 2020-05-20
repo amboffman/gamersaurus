53558cb2ecca2c2cd139a7962088b9050c5ad6cf
mcerrillo1 <mcerrillo1@yahoo.com>
2020-05-19 19:42:14 -0700

    search input initial code

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -4,11 +4,37 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../utils/auth";
 
 function Search() {
-    
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState("");
+  
+  useEffect(() => {
+    const fullSearch = `fields *; limit 15; search "${searchQuery}";
+    `
+    API.fetchGames(fullSearch)
+    .then((response)=>{setSearchResults(response) 
+  });
+  }, [searchQuery]);
+
+
+  function handleFormSubmit (event) {
+    const { value } = event.target;
+    event.preventDefault();
+    setSearchQuery(value)
+  };
+  
+  console.log(searchResults);
+  
   return (
     <div className="container Search">
       <h1>On the Search page!</h1>
       <Link to="/">Go home</Link>
+      <form>
+          <input 
+          name="search"
+          placeholder="Search"
+          />
+          <button onClick= {handleFormSubmit} type="button" >Submit!</button>
+      </form>
     </div>
   );
 }
