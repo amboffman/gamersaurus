84c876a1437805af6d51c567244c0b95eb1fddc4
Samwise <samuelfoster0@gmail.com>
2020-05-20 10:55:09 -0700

    Adds some components to GameInfo page. Creates new component GamerBanner

---
+++ client/src/components/InfoCard/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function index() {
+  return (
+    <li>
+      <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+        <h3 className="uk-card-title">Default</h3>
+        <p>
+          Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing
+          elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua.
+        </p>
+      </div>
+    </li>
+  );
+}
--- client/src/pages/GameInfo/index.js
+++ client/src/pages/GameInfo/index.js
@@ -2,13 +2,27 @@ import React, { useState, useEffect } from "react";
 import API from "../../utils/API";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../utils/auth";
+import InfoCard from "../../components/InfoCard";
+import Carousel from "../../components/Carousel/Carousel"
+import "./style.css";
 
 function GameInfo() {
-    
   return (
-    <div className="container GameInfo">
-      <h1>On the Game Info page!</h1>
-      <Link to="/">Go home</Link>
+    <div className="uk-container GameInfo">
+      <Link id="closeButton" to="/">
+        Go home
+      </Link>
+      <div className="">
+        <img
+          id="coverImage"
+          src="https://straffordchiropractic.com/wp-content/uploads/2017/04/poster-placeholder-203x300.png"
+        />
+        <Carousel>
+          <InfoCard/>
+          <InfoCard/>
+          <InfoCard/>
+        </Carousel>
+      </div>
     </div>
   );
 }
