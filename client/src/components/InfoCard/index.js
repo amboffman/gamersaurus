9974d2d7088e4e3bf328272f942787177ab1f6c1
Samwise <samuelfoster0@gmail.com>
2020-05-20 10:13:05 -0700

    move contents of carousel cards to new card component.

---
+++ client/src/components/InfoCard/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function index() {
+  return (
+    <li>
+      <div className="uk-card uk-card-default">
+        <div className="uk-card-media-top">
+          <img
+            src="https://straffordchiropractic.com/wp-content/uploads/2017/04/poster-placeholder-203x300.png"
+            alt="placeholder"
+          ></img>
+        </div>
+        <div className="uk-card-body">
+          <h3 className="uk-card-title">Game Title</h3>
+        </div>
+      </div>
+    </li>
+  );
+}
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -5,6 +5,7 @@ import "./home.css";
 import { useAuth } from "../../utils/auth";
 import API from "../../utils/API";
 import Carousel from "../../components/Carousel/Carousel";
+import InfoCard from "../../components/InfoCard"
 
 
 function Home() {
@@ -35,7 +36,11 @@ function Home() {
 
   return (
     <div className="App">
-      <Carousel/>
+      <Carousel>
+        <InfoCard/>
+        <InfoCard/>
+        <InfoCard/>
+      </Carousel>
       <p className="App-intro">
         <button
           type="button"
