69a259425193cf92c4e59b03e0f99244e9022419
Samwise <samuelfoster0@gmail.com>
2020-05-20 14:31:53 -0700

    Creates the InfoCard to show the summary.

--- client/src/components/InfoCard/index.js
+++ client/src/components/InfoCard/index.js
@@ -1,16 +1,45 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
 
-export default function index() {
+export default function index(props) {
   return (
     <li>
       <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m">
-        <h3 className="uk-card-title">Default</h3>
-        <p>
-          Lorem ipsum sit amet, consectetur adipiscing
-          elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-          aliqua.
-        </p>
+        <h3 className="uk-card-title">Summary</h3>
+        <p className="overflow-ellipsis summary"> </p>
+
+        <div className="toggle">
+          <p>
+            Escape to a deserted island and create your own paradise as you
+            explore, create, and customize in the Animal Crossing: New Horizons
+            game. Your island getaway
+            <span className="toggle readMore" uk-toggle="target: .toggle">
+              ...Read More
+            </span>
+          </p>
+        </div>
+
+        <div hidden className="toggle">
+          <p>
+            Escape to a deserted island and create your own paradise as you
+            explore, create, and customize in the Animal Crossing: New Horizons
+            game. Your island getaway has a wealth of natural resources that can
+            be used to craft everything from tools to creature comforts. You can
+            hunt down insects at the crack of dawn, decorate your paradise
+            throughout the day, or enjoy sunset on the beach while fishing in
+            the ocean. The time of day and season match real life, so each day
+            on your island is a chance to check in and find new surprises all
+            year round.
+          </p>
+          <span hidden className="toggle readMore" uk-toggle="target: .toggle">
+            Read Less
+          </span>
+        </div>
+
+        <p className="subCategory">Rated</p>
+        <span>E</span>
+        <p className="subCategory">Platforms</p>
+        <span>🎮</span>
       </div>
     </li>
   );
--- client/src/pages/GameInfo/index.js
+++ client/src/pages/GameInfo/index.js
@@ -3,26 +3,27 @@ import API from "../../utils/API";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../utils/auth";
 import InfoCard from "../../components/InfoCard";
-import Carousel from "../../components/Carousel/Carousel"
-import GameBanner from "../../components/GameBanner"
+import Carousel from "../../components/Carousel/Carousel";
+import GameBanner from "../../components/GameBanner";
 import "./style.css";
 
 function GameInfo() {
   return (
     <div className="GameInfo">
-      <Link id="closeButton" to="/">
-        Go home
-      </Link>
+      <span id="closeButton">
+        <Link to="/">✖</Link>
+      </span>
       <div className="">
-        <img className="uk-align-center"
+        <img
+          className="uk-align-center"
           id="coverImage"
           src="https://straffordchiropractic.com/wp-content/uploads/2017/04/poster-placeholder-203x300.png"
         />
-        <GameBanner/>
+        <GameBanner />
         <Carousel>
-          <InfoCard/>
-          <InfoCard/>
-          <InfoCard/>
+          <InfoCard />
+          <InfoCard />
+          <InfoCard />
         </Carousel>
       </div>
     </div>
