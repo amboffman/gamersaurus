07a0be89867ffa5a2a5e6d94cb0d149b6dafe6e8
mcerrillo1 <mcerrillo1@yahoo.com>
2020-05-21 18:49:18 -0700

    create search components, update search.js

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -7,30 +7,70 @@ function Search() {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState("");
 
-function handleFormSubmit (event) {
-  event.preventDefault()
-  console.log("Search", searchQuery)
-  const fullSearch = `fields *; limit 15; search "${searchQuery}";`
-  API.fetchGames(fullSearch)
-  .then((response)=>{setSearchResults(response.data)
-});
-}
+  function handleFormSubmit(event) {
+    event.preventDefault()
+    console.log("Search", searchQuery)
+    const fullSearch = `fields *; limit 15; search "${searchQuery}";`
+    API.fetchGames(fullSearch)
+      .then((response) => { setSearchResults(response.data) })
+      //test console for game responses
+      .then(() => { console.log(searchResults) });
+  }
   return (
-    <div className="container Search">
+    <div class="uk-margin .uk-align-center">
       <h1>On the Search page!</h1>
       <Link to="/">Go home</Link>
-      <form>
-          <input 
+      <form class="uk-search uk-search-default">
+        <span uk-search-icon></span>
+        <input
+          class="uk-search-input"
           name="search"
-          type= "text"
+          type="text"
           placeholder="Search"
           value={searchQuery}
           onChange={e => setSearchQuery(e.target.value)}
-          />
-          <button onClick={handleFormSubmit} type="button" >Go!</button>
+        />
+        <button onClick={handleFormSubmit} type="button" >Go!</button>
       </form>
+
+      <div class="uk-grid-column-small uk-grid-row-large uk-child-width-1-3@s uk-text-center" uk-grid>
+        <div>
+          <div class="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+        <div>
+          <div class="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+        <div>
+          <div class="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+        <div>
+          <div class="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+        <div>
+          <div class="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+        <div>
+          <div class="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+      </div>
     </div>
   );
 }
+
+
+{/* <div className="container Search">
+  <h1>On the Search page!</h1>
+  <Link to="/">Go home</Link>
+  <form>
+      <input 
+      name="search"
+      type= "text"
+      placeholder="Search"
+      value={searchQuery}
+      onChange={e => setSearchQuery(e.target.value)}
+      />
+      <button onClick={handleFormSubmit} type="button" >Go!</button>
+  </form>
+</div> */}
 
 export default Search;
