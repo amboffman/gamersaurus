1ee854fd3e7d1e20f15ab989e0b0777897baa22c
Samwise <samuelfoster0@gmail.com>
2020-05-21 18:44:44 -0700

    Move styles to new css framework in signup and login

--- controllers/igdb.js
+++ controllers/igdb.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const axios = require("axios");
-const IGDBAPI = require("../utils/igdbAPI")
+// const axios = require("axios");
+const IGDBAPI = require("../utils/igdbAPI");
 
 const router = express.Router();
 // ROUTES NEEDED
 // Trending Games with title, rating & cover
 // Coming Soon with title, rating & cover
-// 
 
 router.get("/igdbgames", (req, res) => {
   IGDBAPI.fetchgames(req.query.q)
@@ -17,7 +16,7 @@ router.get("/igdbgames", (req, res) => {
     .catch((error) => {
       // handle errors
       console.log(error);
-      console.log(error.request.url)
+      console.log(error.request.url);
       if (error.response) {
         res.sendStatus(error.response.status);
       } else {
@@ -27,111 +26,3 @@ router.get("/igdbgames", (req, res) => {
 });
 
 module.exports = router;
-
-// const fakeIGDBData = [
-//   {
-//       "id": 115278,
-//       "age_ratings": [
-//           25494
-//       ],
-//       "aggregated_rating": 86.6666666666667,
-//       "aggregated_rating_count": 4,
-//       "alternative_names": [
-//           23839
-//       ],
-//       "category": 0,
-//       "collection": 767,
-//       "cover": 93448,
-//       "created_at": 1550016000,
-//       "first_release_date": 1564012800,
-//       "franchise": 546,
-//       "franchises": [
-//           546
-//       ],
-//       "game_modes": [
-//           1
-//       ],
-//       "genres": [
-//           12
-//       ],
-//       "hypes": 1,
-//       "involved_companies": [
-//           74454,
-//           74455,
-//           80493
-//       ],
-//       "keywords": [
-//           1039,
-//           1323,
-//           1868
-//       ],
-//       "name": "Rune Factory 4 Special",
-//       "platforms": [
-//           130
-//       ],
-//       "player_perspectives": [
-//           2,
-//           3
-//       ],
-//       "popularity": 2090.622837928576,
-//       "pulse_count": 20,
-//       "release_dates": [
-//           165088,
-//           185024,
-//           185025
-//       ],
-//       "screenshots": [
-//           284045,
-//           284047,
-//           284048,
-//           284049,
-//           355347,
-//           355348,
-//           355349,
-//           355350,
-//           355351,
-//           355352
-//       ],
-//       "similar_games": [
-//           28010,
-//           54775,
-//           55199,
-//           81249,
-//           96217,
-//           101608,
-//           103168,
-//           103303,
-//           106987,
-//           119171
-//       ],
-//       "slug": "rune-factory-4-special",
-//       "summary": "Experience this legendary fantasy adventure like never before and embark on exciting escapades with your favorite characters in the brand new Newlywed Mode!",
-//       "tags": [
-//           1,
-//           17,
-//           268435468,
-//           536871951,
-//           536872235,
-//           536872780
-//       ],
-//       "themes": [
-//           1,
-//           17
-//       ],
-//       "total_rating": 86.6666666666667,
-//       "total_rating_count": 4,
-//       "updated_at": 1589846400,
-//       "url": "https://www.igdb.com/games/rune-factory-4-special",
-//       "version_parent": 6874,
-//       "version_title": "Special",
-//       "videos": [
-//           24922,
-//           28296,
-//           34176
-//       ],
-//       "websites": [
-//           113100
-//       ]
-//   }
-// ]
-// console.log(createGameFromIGDBData(fakeIGDBData))
