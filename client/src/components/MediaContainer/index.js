200e3d5579ffd5f20f3831253d48e1e4b959678a
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-05-23 14:46:30 -0700

    refractor MediaContainer

--- client/src/components/MediaContainer/index.js
+++ client/src/components/MediaContainer/index.js
@@ -5,20 +5,25 @@ import ScreenshotsCard from "../ScreenshotsCard/index";
 function MediaContainer(props) {
   let screenshots;
   if (props.screenshots) {
+    console.log("props", props.screenshots);
     screenshots = props.screenshots.map((g) => {
-     return( <ScreenshotsCard
-      key={g.id}
-      imageId={g.image_id}
-    />) 
+     return( {
+       id: g.id,
+       imageId: g.image_id
+     }) 
     });
   }
+  console.log("screenshots:", screenshots);
+
     return (
-        <div class="uk-card uk-card-default uk-card-body uk-width-1-1">
-            <h3 class="uk-card-title">Screenshots</h3>
+        <div className="uk-card uk-card-default uk-card-body uk-width-1-1">
+            <h3 className="uk-card-title">Screenshots</h3>
             <MediaCarousel>
-            {screenshots}
+              {screenshots && screenshots.map((game)=> (
+                <ScreenshotsCard key= {game.id} imageId = {game.imageId}/>
+              ))}
             </MediaCarousel>
-            <h3 class="uk-card-title">Trailer</h3>
+            <h3 className="uk-card-title">Trailer</h3>
         </div>
     )
 }
