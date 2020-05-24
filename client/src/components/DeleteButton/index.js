fc4bc3727e11f909ea48cac5a607c471557c244c
Samwise <samuelfoster0@gmail.com>
2020-05-23 19:47:25 -0700

    Styles the remove button

--- client/src/components/DeleteButton/index.js
+++ client/src/components/DeleteButton/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./style.css"
 
 export default function index(props) {
     return (
         <div>
-            <button data-id={props.id} onClick={props.onClick}>Remove Favorite</button>
+            <button className="deleteButton" data-id={props.id} onClick={props.onClick}>Remove Favorite</button>
         </div>
     )
 }
