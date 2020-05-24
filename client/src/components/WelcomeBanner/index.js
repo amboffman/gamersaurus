11a3d9fc1f8a384afe1ec50f63f7c4331d5df548
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-05-23 19:57:26 -0700

    add dino gif to welcome banner

--- client/src/components/WelcomeBanner/index.js
+++ client/src/components/WelcomeBanner/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import logo from "../../images/dino.gif";
 
 function WelcomeBanner() {
   return (
@@ -7,7 +8,8 @@ function WelcomeBanner() {
       <div className="uk-child-width-expand@s uk-text-center" uk-grid>
         <div>
           <div id= "welcome-banner" className="uk-card uk-card-default uk-card-body">
-              Gamersaurus
+          <img src={logo} className="gif" alt="8-bit dino logo"></img>
+              <span id= "brand-name">Gamersaurus</span>
           </div>
         </div>
       </div>
