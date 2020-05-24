dab4eef05729f471c9e109291b2015686827302f
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-05-23 19:22:13 -0700

    change backgroun color to purple

--- client/src/components/WelcomeBanner/index.js
+++ client/src/components/WelcomeBanner/index.js
@@ -6,7 +6,7 @@ function WelcomeBanner() {
     <div>
       <div className="uk-child-width-expand@s uk-text-center" uk-grid>
         <div>
-          <div className="uk-card uk-card-default uk-card-body">
+          <div id= "welcome-banner" className="uk-card uk-card-default uk-card-body">
               Gamersaurus
           </div>
         </div>
