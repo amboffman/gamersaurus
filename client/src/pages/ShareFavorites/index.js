d82ee48792e8e88293dc4fcaa89f48b1478c7159
Samwise <samuelfoster0@gmail.com>
2020-06-07 11:49:54 -0700

    Initializes the share favorites folder

---
+++ client/src/pages/ShareFavorites/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import API from "../../utils/API";
+import GameResults from "../../components/GameResults";
+import GameCard from "../../components/GameCard";
+
+export default function index() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
