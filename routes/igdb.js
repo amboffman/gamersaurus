e4b84c0df450c8280b688338eddff8e87e787d58
Anthony Boffman <amarrokalboffman@gmail.com>
2020-12-07 19:09:09 -0800

    Fix upgrades IGDB API to v4

--- routes/igdb.js
+++ routes/igdb.js
@@ -6,25 +6,33 @@ const router = express.Router();
 
 router.get("/igdbgames", (req, res) => {
   return axios({
-    url: "https://api-v3.igdb.com/games",
+    url: `https://id.twitch.tv/oauth2/token?client_id=${process.env.Client_ID}&client_secret=${process.env.Client_Secret}&grant_type=client_credentials`,
     method: "POST",
     headers: {
       Accept: "application/json",
-      "user-key": process.env.API_Key,
     },
-    data: req.query.q,
   })
-    .then((response) => {
-      return response.data;
-    })
-    .then((gameData) => {
-      const games = IGDBAPI.createGamesFromIGDBData(gameData);
-      return res.json(games);
+    .then((results) => {
+      return axios({
+        url: "https://api.igdb.com/v4/games",
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Client-ID": process.env.Client_ID,
+          Authorization: `Bearer ${results.data.access_token}`,
+        },
+        data: req.query.q,
+      })
+        .then((response) => {
+          return response.data;
+        })
+        .then((gameData) => {
+          const games = IGDBAPI.createGamesFromIGDBData(gameData);
+          return res.json(games);
+        });
     })
     .catch((error) => {
       // handle errors
-      console.log(error);
-      console.log(error.request.url);
       if (error.response) {
         res.sendStatus(error.response.status);
       } else {
@@ -35,11 +43,13 @@ router.get("/igdbgames", (req, res) => {
 
 router.get("/igdbgame/:id", (req, res) => {
   return axios({
-    url: "https://api-v3.igdb.com/games",
+    url: "https://api.igdb.com/v4/games",
     method: "POST",
     headers: {
       Accept: "application/json",
-      "user-key": process.env.API_Key,
+      // "user-key": process.env.API_Key,
+      "Client-ID": process.env.Client_ID,
+      Authorization: process.env.Authorization,
     },
     data: req.query.q,
   })
