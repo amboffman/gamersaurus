9eda5580aa166b83aeb1db14a4b11b9e2debaeaa
Anthony Boffman <amarrokalboffman@gmail.com>
2020-12-14 16:17:41 -0800

    Feat alerts user in case of signup error

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, Redirect, useHistory } from "react-router-dom";
 import API from "../../utils/API";
 import { useAuth } from "../../utils/auth";
+import { Alert } from "reactstrap";
 import "./style.css";
 
 function Signup() {
@@ -11,6 +12,8 @@ function Signup() {
     password: "",
   });
 
+  const [alert, setAlert] = useState(false);
+  const onDismiss = () => setAlert(false)
   const { isLoggedIn } = useAuth();
 
   const history = useHistory();
@@ -27,7 +30,9 @@ function Signup() {
         // send them to the login page
         history.replace("/login");
       })
-      .catch((err) => {});
+      .catch((err) => {
+        setAlert(true);
+      });
   };
 
   const handleChange = (event) => {
@@ -39,50 +44,54 @@ function Signup() {
   };
 
   return (
-    <div className="uk-container uk-align-center form-container">
-      <h1 className= "header">Sign Up</h1>
-      <form onSubmit={handleFormSubmit}>
-        <div className="uk-margin">
-          <input
-            className="uk-input form-input"
-            placeholder="USERNAME"
-            name="username"
-            type="text"
-            id="username"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="uk-margin">
-          <input
-            className="uk-input form-input"
-            placeholder="EMAIL"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="uk-margin">
-          <input
-            className="uk-input form-input"
-            placeholder="PASSWORD"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit" className="uk-button btn">
-          Submit
-        </button>
-      </form>
+    <>
+      <div className="uk-container uk-align-center form-container">
+        <Alert color="info" className="mt-3 text-center" isOpen={alert} toggle={onDismiss}>Something went wrong creating your account</Alert>
+        <h1 className="header">Sign Up</h1>
 
-      <p>
-        <Link id="link" to="/login">
-          Go to Login
+        <form onSubmit={handleFormSubmit}>
+          <div className="uk-margin">
+            <input
+              className="uk-input form-input"
+              placeholder="USERNAME"
+              name="username"
+              type="text"
+              id="username"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="uk-margin">
+            <input
+              className="uk-input form-input"
+              placeholder="EMAIL"
+              name="email"
+              type="email"
+              id="email"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="uk-margin">
+            <input
+              className="uk-input form-input"
+              placeholder="PASSWORD"
+              name="password"
+              type="password"
+              id="pwd"
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="uk-button btn">
+            Submit
+        </button>
+        </form>
+
+        <p>
+          <Link id="link" to="/login">
+            Go to Login
         </Link>
-      </p>
-    </div>
+        </p>
+      </div>
+    </>
   );
 }
 
